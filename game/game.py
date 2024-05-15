720feababeb1b0990b903dd04627f630f083d308
shepherdp <shepherd.berea@gmail.com>
2024-05-15 17:50:05 -0400

    Bulk drawing, test assets added

--- game/game.py
+++ game/game.py
@@ -2,6 +2,14 @@ import pygame as pg
 import sys
 from .world import World
 from .settings import TILE_SIZE
+import random
+
+from math import sin, pi
+
+WORLD_W = 20
+WORLD_H = 20
+
+DRAW_GRID = True
 
 class Game:
 
@@ -12,7 +20,7 @@ class Game:
 
         self.width, self.height = self.screen.get_size()
 
-        self.world = World(10, 10, self.width, self.height)
+        self.world = World(WORLD_W, WORLD_H, self.width, self.height)
 
     def run(self):
         self.playing = True
@@ -38,15 +46,33 @@ class Game:
     def draw(self):
         self.screen.fill((0, 0, 0))
 
+        self.screen.blit(self.world.surface, (0, 0))
+
         for x in range(self.world.grid_length_x):
             for y in range(self.world.grid_length_y):
 
-                sq = self.world.world[x][y]['cart_rect']
-                rect = pg.Rect(sq[0][0], sq[0][1], TILE_SIZE, TILE_SIZE)
-                pg.draw.rect(self.screen, (0, 0, 255), rect, 1)
+                r_pos = self.world.world[x][y]['render_pos']
+                # self.screen.blit(self.world.tiles['block'],
+                #                  (r_pos[0] + self.width/2,
+                #                   r_pos[1] + self.height/4)
+                #                  )
 
-                poly = self.world.world[x][y]['iso_poly']
-                poly = [(x + self.width / 2, y + self.height / 4) for x, y in poly]
-                pg.draw.polygon(self.screen, (255, 0, 0), poly, 1)
+                if DRAW_GRID:
+                    # put isometric grid on screen
+                    poly = self.world.world[x][y]['iso_poly']
+                    poly = [(x + self.width / 2, y + self.height / 4) for x, y in poly]
+                    pg.draw.polygon(self.screen, (255, 0, 0), poly, 1)
+
+                t = self.world.world[x][y]['tile']
+                offset = 0
+                if t == 'rock':
+                    offset = self.world.tiles[t].get_height() - TILE_SIZE
+                if t == 'tree':
+                    offset = (self.world.tiles[t].get_height() - 3*TILE_SIZE/4)
+                if t:
+                    self.screen.blit(self.world.tiles[t],
+                                     (r_pos[0] + self.width / 2 + TILE_SIZE / 2,
+                                      r_pos[1] + self.height / 4 - offset)
+                                     )
 
         pg.display.flip()
--- game/world.py
+++ game/world.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from .settings import TILE_SIZE
+import random
 
 class World:
 
@@ -8,11 +9,15 @@ class World:
         self.grid_length_y = grid_length_y
         self.width = width
         self.height = height
+        self.tiles = self.load_images()
+
+        self.surface = pg.Surface((width, height))
 
         self.world = self.create_world()
 
     def create_world(self):
 
+        # conrtainer for objects representing world spaces
         world = []
 
         for x in range(self.grid_length_x):
@@ -20,6 +25,10 @@ class World:
             for y in range(self.grid_length_y):
                 tile = self.grid_to_world(x, y)
                 world[x].append(tile)
+
+                render_pos = tile['render_pos']
+                self.surface.blit(self.tiles['block'], (render_pos[0] + self.width / 2,
+                                                        render_pos[1] + self.height / 4))
 
         return world
 
@@ -31,10 +40,23 @@ class World:
 
         iso_poly = [self.cart_to_iso(x, y) for x, y in rect]
 
+        minx = min([x for x, y in iso_poly])
+        miny = min([y for x, y in iso_poly])
+
+        r = random.random()
+        if r < .05:
+            tile = 'tree'
+        elif r < .1:
+            tile = 'rock'
+        else:
+            tile = ''
+
         out = {
             'grid': [x, y],
             'cart_rect': rect,
             'iso_poly': iso_poly,
+            'render_pos': [minx, miny],
+            'tile': tile
         }
 
         return out
@@ -43,3 +65,13 @@ class World:
         iso_x = x - y
         iso_y = (x + y) / 2
         return iso_x, iso_y
+
+    def load_images(self):
+
+        block = pg.image.load('assets\\graphics\\testblock.png')
+        rock = pg.image.load('assets\\graphics\\testrock.png')
+        tree = pg.image.load('assets\\graphics\\testtree.png')
+
+        return {'block': block,
+                'rock': rock,
+                'tree': tree}
